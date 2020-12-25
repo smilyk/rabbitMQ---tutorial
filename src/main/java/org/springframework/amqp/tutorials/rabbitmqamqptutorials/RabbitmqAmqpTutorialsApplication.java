ef1f6ac82dc8b07d51e5c5a9cd3d8c4a2ff7350b
smilyk <smilyk1982@gmail.com>
2020-12-25 12:27:35 +0200

    part1 hello word

---
+++ src/main/java/org/springframework/amqp/tutorials/rabbitmqamqptutorials/RabbitmqAmqpTutorialsApplication.java
@@ -0,0 +1,33 @@
+package org.springframework.amqp.tutorials.rabbitmqamqptutorials;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class RabbitmqAmqpTutorialsApplication {
+	@Profile("usage_message")
+	@Bean
+	public CommandLineRunner usage() {
+		return args -> {
+			System.out.println("This app uses Spring Profiles to сontrol its behavior.\n");
+					System.out.println("Sample usage: java -jar rabbit-tutorials.jar " +
+							"--spring.profiles.active=hello-world,sender");
+		};
+	}
+
+	@Profile("!usage_message")
+	@Bean
+	public CommandLineRunner tutorial() {
+		return new RabbitAmqpTutorialsRunner();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqAmqpTutorialsApplication.class, args);
+	}
+}
+
